d4636fe6f6e2083e5ffc49f13a3a2e5cb9c9271d
ViestursLinins <114301934+ViestursLinins@users.noreply.github.com>
2023-12-29 15:18:39 +0200

    Add files via upload

---
+++ skaitli.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void){
+printf("Skaitlu ievade ar int\n");
+
+int a=100000;
+int b=400000;
+
+int c = a*b;
+
+printf("%d\n",c);
+
+
+long int d=100000;
+long int e=400000;
+long int f=e*d;
+printf("Skaitlu ievade  ar long int\n");
+printf("%lu\n",f);
+
+unsigned long int g=100000;
+unsigned long int h=400000;
+unsigned long int i=g*h;
+printf("Skaitlu ievade  ar long int\n");
+printf("%lu\n",f);
+
+}
